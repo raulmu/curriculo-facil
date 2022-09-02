50705c23f111e6187137989bd186fce399a61351
Raul Uhlmann <raulmu@outlook.com>
2022-09-01 23:01:30 -0300

    initial config of firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AuthModule } from "@angular/fire/auth";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { RegistropageComponent } from './registropage/registropage.component';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,9 +26,14 @@ import { RegistropageComponent } from './registropage/registropage.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// https://codesource.io/firebase-authentication-in-angular-using-angularfire/
