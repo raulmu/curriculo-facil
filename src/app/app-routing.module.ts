bffdda87551cc7e634c52a3d9d9b1e3f8bf2c062
Raul Uhlmann <raulmu@outlook.com>
2022-09-05 23:38:54 -0300

    upgrades on auth service and registro page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmailLoginComponent } from './pages/email-login/email-login.component';
+import { EsqueceuSenhaComponent } from './pages/esqueceu-senha/esqueceu-senha.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PainelComponent } from './pages/painel/painel.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { VerificarEmailComponent } from './pages/verificar-email/verificar-email.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'registro', component: RegistroComponent },
+  { path: 'cadastro', component: RegistroComponent },
   { path: 'perfil', component: PerfilComponent},
-  { path: 'email-login', component: EmailLoginComponent}
+  { path: 'login-email', component: EmailLoginComponent},
+  { path: 'painel', component: PainelComponent },
+  { path: 'esqueceu-senha', component: EsqueceuSenhaComponent },
+  { path: 'verificar-email', component: VerificarEmailComponent },
 ];
 
 @NgModule({
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { User } from '../services/user';
 import * as auth from 'firebase/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import {
@@ -11,53 +12,121 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class AuthService {
+  userData: any;
   constructor(
     public afs: AngularFirestore, // Inject Firestore service
     public auth: AngularFireAuth,
-    private router: Router
+    private router: Router,
+    public ngZone: NgZone
   ) {
+    this.auth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
   }
 
-  login(email: string, password: string) {
-    this.auth.signInWithEmailAndPassword(email, password)
-      .then((value) => {
-        console.log('Nice, it worked!');
-        this.router.navigateByUrl('/perfil');
+  signIn(email: string, password: string) {
+    return this.auth
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.setUserData(result.user);
+        this.auth.authState.subscribe((user) => {
+          if (user) {
+            this.router.navigate(['painel']);
+          }
+        });
       })
       .catch((err) => {
         console.log('Something went wrong: ', err.message);
       });
   }
 
-  emailSignup(email: string, password: string) {
-    this.auth.createUserWithEmailAndPassword(email, password)
-      .then((value) => {
-        console.log('Sucess', value);
-        this.router.navigateByUrl('/login');
+  setUserData(user: any) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${user.uid}`
+    );
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      emailVerified: user.emailVerified,
+    };
+    return userRef.set(userData, {
+      merge: true,
+    });
+  }
+
+  signUp(email: string, password: string) {
+    return this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        /* Call the SendVerificaitonMail() function when new user sign
+        up and returns promise */
+        this.sendVerificationMail();
+        this.setUserData(result.user);
       })
       .catch((error) => {
         console.log('Something went wrong: ', error);
       });
   }
 
+  // Send email verfificaiton when new user sign up
+  sendVerificationMail() {
+    return this.auth.currentUser
+      .then((u: any) => u.sendEmailVerification())
+      .then(() => {
+        this.router.navigate(['verificar-email']);
+      });
+  }
+
+  forgotPassword(passwordResetEmail: string) {
+    return this.auth
+      .sendPasswordResetEmail(passwordResetEmail)
+      .then(() => {
+        window.alert('Password reset email sent, check your inbox.');
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
+  }
+
+  //TODO
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user !== null && user.emailVerified !== false ? true : false;
+  }
+
   googleLogin() {
     var provider = new auth.GoogleAuthProvider();
     return this.oAuthLogin(provider)
       .then((value: any) => {
-        console.log('Sucess', value), this.router.navigateByUrl('/perfil');
+        console.log('Sucess', value), this.router.navigate(['painel']);
       })
       .catch((error: any) => {
         console.log('Something went wrong: ', error);
       });
   }
 
-  logout() {
+  signOut() {
     this.auth.signOut().then(() => {
+      localStorage.removeItem('user');
       this.router.navigate(['/']);
     });
   }
 
   private oAuthLogin(provider: any) {
-    return this.auth.signInWithPopup(provider);
+    return this.auth.signInWithPopup(provider).then((result) => {
+      this.setUserData(result.user);
+    })
+    .catch((error) => {
+      console.log('Something went wrong: ', error);
+    });;
   }
 }
