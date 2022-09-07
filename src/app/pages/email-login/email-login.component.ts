f894021383bb252cdf2c7897c018b47e17b9a191
Raul Uhlmann <raulmu@outlook.com>
2022-09-06 22:30:13 -0300

    html and styles from email login registro

--- src/app/pages/email-login/email-login.component.ts
+++ src/app/pages/email-login/email-login.component.ts
@@ -4,13 +4,11 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-email-login',
   templateUrl: './email-login.component.html',
-  styleUrls: ['./email-login.component.scss']
+  styleUrls: ['./email-login.component.scss'],
 })
 export class EmailLoginComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  loginGoogle() {
+  googleLogin() {
       this.authService.googleLogin();
   }
 }
